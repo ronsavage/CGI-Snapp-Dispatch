7bea2f49c3146f9ea070cd49447e8ef8f3626b37
ronsavage <ron@savage.net.au>
2012-04-09 16:01:21 +1000

    Initial version

---
+++ t/lib/CGI/Snapp/Dispatch/PSGITest.pm
@@ -0,0 +1,23 @@
+package CGI::Snapp::Dispatch::PSGITest;
+
+use parent 'CGI::Snapp';
+use strict;
+use warnings;
+
+use Hash::FieldHash ':all';
+
+our $VERSION = '1.00';
+
+# --------------------------------------------------
+
+sub setup
+{
+	my($self) = @_;
+
+	$self -> run_modes(start => sub{return 'Hello World'});
+
+} # End of setup.
+
+# --------------------------------------------------
+
+1;
---
+++ t/load.t
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+
+use CGI::Snapp::Dispatch;
+
+use Test::More tests => 1;
+
+# ------------------------------------------------
+
+BEGIN {use_ok('CGI::Snapp::Dispatch')}
